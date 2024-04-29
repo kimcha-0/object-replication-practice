3dee9734173c50f6caff7ce7b3b9b83fc63299e5
David <davidcha@email.unc.edu>
2024-04-28 23:41:10 -0400

    Done with A4. Adding GIPC.

--- src/comp533/A4Grader.java
+++ src/comp533/A4Grader.java
@@ -11,13 +11,13 @@ public class A4Grader {
 			// not just the ones that failed		
 			GraderBasicsTraceUtility.setBufferTracedMessages(true);
 			// Change this number if a test trace gets longer than 600 and is clipped
-			int maxPrintedTraces = 600;
+			int maxPrintedTraces = 100;
 			GraderBasicsTraceUtility.setMaxPrintedTraces(maxPrintedTraces);
 			// Change this number if all traces together are longer than 2000
 			int maxTraces = 2000;
 			GraderBasicsTraceUtility.setMaxTraces(maxTraces);
 			// Change this number if your process times out prematurely
-			int processTimeOut = 5;
+			int processTimeOut = 45;
 			BasicProjectExecution.setProcessTimeOut(processTimeOut);
 			// You need to always call such a method
 			S24Assignment4Suite.main(args);
--- src/comp533/Main.java
+++
@@ -1,8 +0,0 @@
-package comp533;
-import coupledsims.TwoCoupledHalloweenSimulations;
-
-public class Main {
-	public static void main(String args[]) {
-		
-	}
-}
